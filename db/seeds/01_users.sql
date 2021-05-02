3fc3f943e688789b86246d2547066f9231551eb4
donnyli95 <sz3li@uwaterloo.ca>
2021-05-02 16:22:02 +0000

    sets up SQL DB and preliminary data

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,4 +1,7 @@
 -- Users table seeds here
-INSERT INTO users (name, email, password, phone, street, city, province, country, post_code)
-VALUES ('Donny', 'donny@donny.com', 'password', '123-123-1234', '123 Strawberry St', 'Calgary', 'Alberta', 'Canada', 'T9N 0I7');
+INSERT INTO users (name, email, password, phone)
+VALUES
+  ('Donny', 'donny@donny.com', 'password', '123-123-1234'),
+  ('Michelle', 'michelle@michelle.com', 'password', '123-123-1234'),
+  ('Adrian', 'adrian@adrian.com', 'password', '123-123-1234');
 
---
+++ db/seeds/04_order_submissions.sql
@@ -0,0 +1,12 @@
+INSERT INTO order_submissions (order_id, item_id, quantity)
+VALUES
+  (1, 1, 1),
+  (1, 2, 1),
+  (1, 3, 2),
+  (2, 3, 1),
+  (2, 6, 2),
+  (2, 4, 2),
+  (3, 2, 1),
+  (3, 5, 4),
+  (3, 7, 2);
+
