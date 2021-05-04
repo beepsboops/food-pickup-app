7ed1824c5565267ecbdcda56887e6dd4713124fd
Michelle Verge <michellelverge@gmail.com>
2021-05-04 21:01:03 +0000

    resolves merge conflict

--- routes/pages.js
+++ routes/pages.js
@@ -92,12 +92,10 @@ module.exports = () => {
     //hard coding user id
     getCurrentOrder(1)
       .then((order_id) => {
-        console.log("current order:", order_id);
         return addOrderItem(order_id, itemId, quantity);
       })
       .then(() => res.redirect("/menu"))
       .catch((err) => {
-        console.log(err.message);
         res.status(500).json(err);
       });
   });
