fc0d9cfdb66cc44f66d62e90d4bd5db80ad7ec73
donnyli95 <sz3li@uwaterloo.ca>
2021-05-05 23:36:29 +0000

    styling for order submit page

--- public/scripts/order-submission.js
+++ public/scripts/order-submission.js
@@ -63,6 +63,7 @@ $(document).ready(function() {
 
     const allRows = $('.order-data-row')
     const orderSubmissionData = [{
+
       order_id: Number($(`.order-id`).html().split("#").pop())
     }];
 
