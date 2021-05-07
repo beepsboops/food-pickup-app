c90bde474cd45d9723194fa9faa7652e36cdac52
A <awongpro7@gmail.com>
2021-05-07 02:57:45 +0000

    update send_sms

--- public/scripts/order-status.js
+++ public/scripts/order-status.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
   // change order status
-  $(".wrapper").click(() => {
+  $(".order-status").click(() => {
     // add SQL function to update order status - active to processed? to confirmed?
-    $(".wrapper").replaceWith(
-      "<span>Order Confirmed! You're ETA is 40 minutes! Redirecting to home...</span>"
+    $(".order-status").replaceWith(
+      "<span>Order Confirmed! Your ETA is 40 minutes! Redirecting to home...</span>"
     );
     setTimeout(function () {
       window.location.replace("/");
